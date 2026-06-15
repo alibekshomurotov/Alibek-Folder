c647120703dacc901963309dbbfac8613862e6e1
Alibek <umidbekshomurotov3@gmail.com>
2026-06-15 13:20:53 +0500

    41,1

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,10 +1,5 @@
-"""Database layer initialization"""
+
 from app.database.connection import get_engine, get_session_factory, init_db, close_db
 from app.database.models import Base
 
-__all__ = ["get_engine", "get_session_factory", "init_db", "close_db", "Base"]"""Repository layer initialization"""
-from app.database.repositories.user_repo import UserRepository
-from app.database.repositories.channel_repo import ChannelRepository
-from app.database.repositories.download_repo import DownloadRepository
-
-__all__ = ["UserRepository", "ChannelRepository", "DownloadRepository"]
+__all__ = ["get_engine", "get_session_factory", "init_db", "close_db", "Base"]
